706af0647b62355b594598f6cf6aec7ff2f57b7d
jalcjim1506 <jalcjim1506@m.educaand.es>
2025-03-11 11:01:33 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+let numero_cuadros = 0;
 let columna1 = document.getElementById("columna1");
+
+function sumarCuadro() {
+ let nuevoDiv = document.createElement("div")
+ numero_cuadros = numero_cuadros + 1;
+ nuevoDiv.textContent = "nuevo div" + numero_cuadros
+
+ nuevoDiv.classList.add("cuadro")
+
+ columna1.appendChild(nuevoDiv);
+
+}
+for(let contador = 0; contador < 16; contador = contador + 1 ) {
+  sumarCuadro();
+
+}
+
+
+
+
+let columna2 = document.getElementById("columna2");
+
+function producto() {
+    let nuevoDiv = document.createElement("div")
+    numero_cuadros = numero_cuadros + 1;
+    nuevoDiv.textContent = "nuevo div" + numero_cuadros
+   
+    nuevoDiv.classList.add("cuadro")
+   
+    columna1.appendChild(nuevoDiv);
+}
