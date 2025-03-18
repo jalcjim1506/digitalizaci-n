f4bb22ac0f19d842a163aff5e76e29ae857a5ee0
jalcjim1506 <jalcjim1506@m.educaand.es>
2025-03-18 11:01:24 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ let numero_cuadros = 0;
 let columna1 = document.getElementById("columna1");
 
 function sumarCuadro() {
-  let nuevoDiv = document.createElement("div")
-  let Nombres=["Agua", "Cafe", "Aceite","Fanta de Naranja", "Te", "Nestea", "Coca-Cola", "Aquarius", "Leche", "Manzana", "Huevos (12)", "Arroz", "Patatas", "Lechuga", "sal", "Azucar" ] Precios=["1.5$","2$","5,5$","1,75$","1,25$ ,"2,5$"]  
-  nuevoDiv.textContent = Nombres[numero_cuadros]
+  let Nombres=["Agua", "Cafe", "Aceite","Fanta de Naranja", "Te", "Nestea", "Coca-Cola", "Aquarius", "Leche", "Manzana", "Huevos (12)", "Arroz", "Patatas", "Lechuga", "sal", "Azucar" ];
+  let Precios=[1.5,2,5.5,1.75,1.25,2,2,2,3.5,3,,4.5,2.5,6,3,5 ]; 
+
+
+  let nuevoDivTarjeta = document.createElement("div")
+
+
+ let nuevoDivNombre = document.createElement("div");
+ nuevoDivTarjeta.appendChild(nuevoDivNombre);
+  nuevoDivNombre.textContent = Nombres[numero_cuadros];
+
+  let nuevoDivPrecios
+ 
  
   nuevoDiv.classList.add("cuadro")
  nuevoDiv.addEventListener("click", sumarCarrito.bind(null,numero_cuadros))
   columna1.appendChild(nuevoDiv);
   numero_cuadros = numero_cuadros + 1;
+
+  nuevoDivTarjeta.classList.add("tarjeta")
 
 }
 for(let contador = 0; contador < 16; contador = contador + 1 ) {
@@ -29,11 +41,12 @@ function sumarCarrito(num) {
     
     console.log(Nombres[num])
     nuevoDiv.textContent = Nombres[num]
-   
+   let Precios=[1.5,2,5.5,1.75,1.25,2,2,2,3.5,3,,4.5,2.5,6,3,5 ];
     nuevoDiv.classList.add("cuadro")
    
     columna2.appendChild(nuevoDiv);
     numero_cuadros = numero_cuadros + 1;
+    
     
     
 }
